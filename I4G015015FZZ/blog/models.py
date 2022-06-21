6524c01e4f4f3b74d64f41058570a8e4d93061b5
Ada-ikemefuna <adaezeikemefuna@yahoo.com>
2022-06-21 15:05:43 +0530

    editted models.py

--- I4G015015FZZ/blog/models.py
+++ I4G015015FZZ/blog/models.py
@@ -7,5 +7,5 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField(max_length=20000000)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(default=timezone.now)
